4eaf7c956df59a8f1fc7f92308017bac2070a466
Shobha Nishad <193940807+shobhanishad@users.noreply.github.com>
2025-10-23 18:04:46 +0530

    fd

--- app/Http/Controllers/PricingMaster/PricingMasterViewsController.php
+++ app/Http/Controllers/PricingMaster/PricingMasterViewsController.php
@@ -323,7 +323,7 @@ class PricingMasterViewsController extends Controller
         // Fetch LMPA and LMP data
         $lmpaLookup = collect();
         try {
-            $lmpaLookup = DB::connection('repricer')
+            $lmpaLookup = DB::connection('repricer_5core')
                 ->table('lmpa_data')
                 ->select('sku', DB::raw('MIN(price) as lowest_price'))
                 ->where('price', '>', 0)
@@ -332,12 +332,12 @@ class PricingMasterViewsController extends Controller
                 ->get()
                 ->keyBy('sku');
         } catch (Exception $e) {
-            Log::warning('Could not fetch LMPA data from repricer database: ' . $e->getMessage());
+            Log::warning('Could not fetch LMPA data from repricer_5core database: ' . $e->getMessage());
         }
 
         $lmpLookup = collect();
         try {
-            $lmpLookup = DB::connection('repricer')
+            $lmpLookup = DB::connection('repricer_5core')
                 ->table('lmp_data')
                 ->select('sku', DB::raw('MIN(price) as lowest_price'), 'link')
                 ->where('price', '>', 0)
@@ -346,7 +346,7 @@ class PricingMasterViewsController extends Controller
                 ->get()
                 ->keyBy('sku');
         } catch (Exception $e) {
-            Log::warning('Could not fetch LMP data from repricer database: ' . $e->getMessage());
+            Log::warning('Could not fetch LMP data from repricer_5core database: ' . $e->getMessage());
         }
 
         $processedData = [];
